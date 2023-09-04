3200323a11a084691eb663c6f67ae04f143cd529
akorbutovski <akorbutovski@elinext.com>
2023-09-04 16:53:14 +0300

    forms: replace useState on useRef for best render

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
 import './App.css';
-import {useState} from "react";
-
+import {useRef, useState} from "react";
 
 function App() {
-    const [inputs, setInputs] = useState({
+    // const [inputs, setInputs] = useState({
+    console.log('render')
+    const inputs = useRef({
         name: '',
         lastname: '',
         middname: '',
     });
 
     const handler = (e) => {
-        if (e.target.value.includes('a')) {
-            setInputs((prevState) => ({
-                ...prevState,
-                [e.target.name]: prevState[e.target.name] + '-'
-            }))
-        } else {
-            setInputs((prevState) => ({
-                ...prevState,
+            inputs.current = {
+                ...inputs.current,
                 [e.target.name]: e.target.value
-            }))
-        }
-
+            }
     }
     const submit = () => {
-        console.log('submit: ', inputs)
-        setInputs({
-            name: '',
-            lastname: '',
-            middname: '',
-        });
+        console.log('submit: ', inputs.current)
     }
 
   return (
     <div className="App">
       <header className="App-header">
-          <input value={inputs.name} placeholder='name' name='name' type="text" onChange={handler}/>
-          <input value={inputs.lastname} placeholder='lastname' name='lastname' type="text" onChange={handler}/>
-          <input value={inputs.middname} placeholder='middname' name='middname' type="text" onChange={handler}/>
+          <input placeholder='name' name='name' type="text" onChange={handler}/>
+          <input placeholder='lastname' name='lastname' type="text" onChange={handler}/>
+          <input placeholder='middname' name='middname' type="text" onChange={handler}/>
           <button onClick={submit}>Submit</button>
       </header>
     </div>
