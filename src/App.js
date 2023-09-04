31f5902a09b0355180f4d3955f702483787ebe3c
akorbutovski <akorbutovski@elinext.com>
2023-09-04 16:15:49 +0300

    forms: multi fields inputs & theirs handlers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
+    const [inputs, setInputs] = useState({
+        name: '',
+        lastname: '',
+        middname: '',
+    });
+
+    const handler = (e) => {
+        if (e.target.value.includes('a')) {
+            setInputs((prevState) => ({
+                ...prevState,
+                [e.target.name]: prevState[e.target.name] + '-'
+            }))
+        } else {
+            setInputs((prevState) => ({
+                ...prevState,
+                [e.target.name]: e.target.value
+            }))
+        }
+
+    }
+    const submit = () => {
+        console.log('submit: ', inputs)
+        setInputs({
+            name: '',
+            lastname: '',
+            middname: '',
+        });
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <input value={inputs.name} placeholder='name' name='name' type="text" onChange={handler}/>
+          <input value={inputs.lastname} placeholder='lastname' name='lastname' type="text" onChange={handler}/>
+          <input value={inputs.middname} placeholder='middname' name='middname' type="text" onChange={handler}/>
+          <button onClick={submit}>Submit</button>
       </header>
     </div>
   );
