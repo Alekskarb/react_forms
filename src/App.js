3fbfa58e2d7ade40a23c0db9766fcc52d17e8516
Alex Korbutovski <akorbutovski@elinext.com>
2023-09-05 15:57:34 +0300

    forms: focus & blur events

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import {useRef, useState} from "react";
 function App() {
     console.log('render')
 
-    const formRef = useRef()
+    const formRef = useRef(null);
+    const inputRef = useRef(null);
 
     const [inputs, setInputs] = useState({
-    // const inputs = useRef({
-    //     name: '',
-    //     lastname: '',
-    //     middname: '',
+        // const inputs = useRef({
+        //     name: '',
+        //     lastname: '',
+        //     middname: '',
     });
     const handler = (e) => {
         // inputs.current = {
         //     ...inputs.current,
         //     [e.target.name]: e.target.value
         // }
-        console.log('event: ', e)
-
-        setInputs(prevState => ({
-            ...prevState,
-            [e.target.name]: e.target.value
-        }))
+        if (e.target.value.includes('q')) {
+            inputRef.current.blur()
+        } else {
+            console.log('event: ', e)
+            setInputs(prevState => ({
+                ...prevState,
+                [e.target.name]: e.target.value
+            }))
+        }
     }
     const submit = (e) => {
         // console.log('submit: ', inputs.current)
@@ -35,23 +39,30 @@ function App() {
         console.log('reset: ')
         setInputs({})
     }
+    const fillOut = () => {
+        console.log('reset: ')
+        inputRef.current.focus();
+    }
 
     return (
-    <div className="App">
-      <header className="App-header">
-          {/*<form action="http://amd.by">*/}
-          <form ref={formRef} action="" onChange={handler}
-                onSubmit={submit} onReset={reset}>
-              <input placeholder='name' name='name' type="text" />
-              <input placeholder='lastname' name='lastname' type="text"/>
-              <input placeholder='middname' name='middname' type="text"/>
-              {/*<button onClick={submit}>Submit</button>*/}
-              <button type="submit">Submit</button>
-              <button type="reset">Reset</button>
-          </form>
-      </header>
-    </div>
-  );
+        <div className="App">
+            <header className="App-header">
+                {/*<form action="http://amd.by">*/}
+                <button onClick={fillOut}>Fill out</button>
+                <form ref={formRef} action="" onChange={handler}
+                      onSubmit={submit} onReset={reset}>
+                    <input ref={inputRef} onFocus={() => console.log("focus")}
+                           onBlur={() => console.log("blur")}
+                           placeholder='name' name='name' type="text"/>
+                    <input placeholder='lastname' name='lastname' type="text"/>
+                    <input placeholder='middname' name='middname' type="text"/>
+                    {/*<button onClick={submit}>Submit</button>*/}
+                    <button type="submit">Submit</button>
+                    <button type="reset">Reset</button>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
