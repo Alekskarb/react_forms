8dde2b5f1bccab98f1d6df3f8e65a57eef4cd85d
Alex Korbutovski <akorbutovski@elinext.com>
2023-09-05 14:59:46 +0300

    forms: reset & submit events

--- src/App.js
+++ src/App.js
@@ -2,31 +2,53 @@ import './App.css';
 import {useRef, useState} from "react";
 
 function App() {
-    // const [inputs, setInputs] = useState({
     console.log('render')
-    const inputs = useRef({
-        name: '',
-        lastname: '',
-        middname: '',
+
+    const formRef = useRef()
+
+    const [inputs, setInputs] = useState({
+    // const inputs = useRef({
+    //     name: '',
+    //     lastname: '',
+    //     middname: '',
     });
-
     const handler = (e) => {
-            inputs.current = {
-                ...inputs.current,
-                [e.target.name]: e.target.value
-            }
+        // inputs.current = {
+        //     ...inputs.current,
+        //     [e.target.name]: e.target.value
+        // }
+        console.log('event: ', e)
+
+        setInputs(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
     }
-    const submit = () => {
-        console.log('submit: ', inputs.current)
+    const submit = (e) => {
+        // console.log('submit: ', inputs.current)
+        e.preventDefault()
+        console.log('submit: ', inputs)
+        formRef.current.reset()
+        // reset()
+    }
+    const reset = () => {
+        console.log('reset: ')
+        setInputs({})
     }
 
-  return (
+    return (
     <div className="App">
       <header className="App-header">
-          <input placeholder='name' name='name' type="text" onChange={handler}/>
-          <input placeholder='lastname' name='lastname' type="text" onChange={handler}/>
-          <input placeholder='middname' name='middname' type="text" onChange={handler}/>
-          <button onClick={submit}>Submit</button>
+          {/*<form action="http://amd.by">*/}
+          <form ref={formRef} action="" onChange={handler}
+                onSubmit={submit} onReset={reset}>
+              <input placeholder='name' name='name' type="text" />
+              <input placeholder='lastname' name='lastname' type="text"/>
+              <input placeholder='middname' name='middname' type="text"/>
+              {/*<button onClick={submit}>Submit</button>*/}
+              <button type="submit">Submit</button>
+              <button type="reset">Reset</button>
+          </form>
       </header>
     </div>
   );
